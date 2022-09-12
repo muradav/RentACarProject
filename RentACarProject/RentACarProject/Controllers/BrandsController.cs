fc136ca55ec0bcbe79e7b29aa889145eec84bc32
muradav <muradav@code.edu.az>
2022-09-12 15:54:20 +0400

    feat: update and delete methods added to BrandsController

--- RentACarProject/RentACarProject/Controllers/BrandsController.cs
+++ RentACarProject/RentACarProject/Controllers/BrandsController.cs
@@ -9,6 +9,7 @@ using RentACarProject.Entities;
 using RentACarProject.Extentions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -57,7 +58,7 @@ namespace RentACarProject.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromForm] BrandCreateDto brandCreateDto)
+        public async Task<IActionResult> Create([FromForm] BrandCreateDto brandCreateDto)
         {
             bool existCategory = _context.Brands.Any(c => c.Name.ToLower() == brandCreateDto.Name.ToLower());
             if (existCategory)
@@ -78,10 +79,52 @@ namespace RentACarProject.Controllers
             newBrand.CreatedAt = DateTime.Now;
             newBrand.ImageUrl = brandCreateDto.Photo.SaveImage(_env, "assets/img/brand");
 
-            _context.Add(newBrand);
-            _context.SaveChanges();
+            await _context.AddAsync(newBrand);
+            await _context.SaveChangesAsync();
 
-            return NoContent();
+            return Ok(newBrand);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id,[FromForm] BrandUpdateDto brandUpdateDto)
+        {
+            Brand b = _context.Brands.FirstOrDefault(b => b.Id == id);
+            if (b == null)
+            {
+                return NotFound();
+            }
+            if (_context.Brands.Any(c => c.Name.ToLower() == brandUpdateDto.Name.ToLower() && c.Id != id))
+            {
+                return BadRequest();
+            }
+
+            if (brandUpdateDto != null)
+            {
+                string path = Path.Combine(_env.WebRootPath, "assets/img/brand", b.ImageUrl);
+                path.DeleteImage();
+                b.ImageUrl = brandUpdateDto.Photo.SaveImage(_env, "assets/img/brand");
+            }
+
+            b.Name = brandUpdateDto.Name;
+
+            await _context.SaveChangesAsync();
+            return StatusCode(200, brandUpdateDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Brand b = _context.Brands.FirstOrDefault(b => b.Id == id);
+            if (b == null)
+            {
+                return NotFound();
+            }
+
+            string path = Path.Combine(_env.WebRootPath, "assets/img/brand", b.ImageUrl);
+            path.DeleteImage();
+            _context.Brands.Remove(b);
+            await _context.SaveChangesAsync();
+            return Ok($"Brand {b.Name} Deleted Successfully.");
         }
     }
 }
---
+++ RentACarProject/RentACarProject/Dtos/BrandDtos/BrandUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace RentACarProject.Dtos.BrandDtos
+{
+    public class BrandUpdateDto
+    {
+        public string Name { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
